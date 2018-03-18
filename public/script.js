80c05ca2098dbb3090252c24bf5988a359cbcd2f
paruyrMelikyan <paruyr.melikyan@tumo.org>
2018-03-18 23:53:08 +0400

    dad

--- public/script.js
+++ public/script.js
@@ -1,13 +1,13 @@
 function genMatrix(w, h) {
     var matrix = [];
-    for(var y = 0; y < h; y++) {
+    for (var y = 0; y < h; y++) {
         matrix[y] = [];
-        for(var x = 0; x < w; x++) {
+        for (var x = 0; x < w; x++) {
             var r = random(100);
-            if     (r < 20) r = 0;
-            else if(r < 65) r = 1;
-            else if(r < 90) r = 2;
-            else if(r < 100)r = 3;
+            if (r < 20) r = 0;
+            else if (r < 65) r = 1;
+            else if (r < 90) r = 2;
+            else if (r < 100) r = 3;
             matrix[y][x] = r;
         }
     }
@@ -25,16 +25,16 @@ function setup() {
     createCanvas(side * w, side * h);
     background("#acacac");
     frameRate(5);
-    for(var y in matrix) {
-        for(var x in matrix[y]) {
-            if(matrix[y][x] == 1) {
-                grassArr.push(new Grass(x*1, y*1, 1));
+    for (var y in matrix) {
+        for (var x in matrix[y]) {
+            if (matrix[y][x] == 1) {
+                grassArr.push(new Grass(x * 1, y * 1, 1));
             }
-            else if(matrix[y][x] == 2) {
-                xotakerArr.push(new Xotaker(x*1, y*1, 2));
+            else if (matrix[y][x] == 2) {
+                xotakerArr.push(new Xotaker(x * 1, y * 1, 2));
             }
-            else if(matrix[y][x] == 3) {
-                gishatichArr.push(new Gishatich(x*1, y*1, 3))
+            else if (matrix[y][x] == 3) {
+                gishatichArr.push(new Gishatich(x * 1, y * 1, 3))
             }
         }
     }
@@ -42,35 +42,35 @@ function setup() {
 
 function draw() {
     background("#acacac");
-    for(var y in matrix) {
-        for(var x in matrix[y]) {
-            if(matrix[y][x] == 0) {
+    for (var y in matrix) {
+        for (var x in matrix[y]) {
+            if (matrix[y][x] == 0) {
                 fill("#acacac");
             }
-            else if(matrix[y][x] == 1) {
+            else if (matrix[y][x] == 1) {
                 fill("green");
             }
-            else if(matrix[y][x] == 2) {
+            else if (matrix[y][x] == 2) {
                 fill("yellow");
             }
-            else if(matrix[y][x] == 3) {
+            else if (matrix[y][x] == 3) {
                 fill("red");
             }
             rect(x * side, y * side, side, side);
         }
     }
 
-    for(var i in grassArr) {
+    for (var i in grassArr) {
         grassArr[i].mul();
     }
 
-    for(var i in xotakerArr) {
+    for (var i in xotakerArr) {
         xotakerArr[i].bazmanal();
         xotakerArr[i].utel();
         xotakerArr[i].mahanal();
     }
 
-    for(var i in gishatichArr) {
+    for (var i in gishatichArr) {
         gishatichArr[i].bazmanal();
         gishatichArr[i].utel();
         gishatichArr[i].mahanal();
